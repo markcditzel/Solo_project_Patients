46bf73f2ee12d6a270674354f166265c87b4bb67
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-07-22 17:36:24 +0100

    created patient.sql file, VARCHAR for dates

---
+++ patients_code/db/patients.sql
@@ -0,0 +1,35 @@
+DROP TABLE patients;
+DROP TABLE diseases;
+DROP TABLE diagnoses;
+
+CREATE TABLE patients
+(
+  id serial8 primary key,
+  first_name VARCHAR(255) not null,
+  second_name VARCHAR(255) not null,
+  dob VARCHAR(10), -- restrain to xx-xx-xxxx
+  age INT1,
+  gender VARCHAR(255),
+  profession VARCHAR(255)
+);
+
+CREATE TABLE diseases
+(
+  id serial8 primary key,
+  common_name VARCHAR(255) not null,
+  latin_name VARCHAR(255),
+  disease_agent_class VARCHAR(255),
+  organs_affected VARCHAR(255),
+  severity_index INT1
+);
+
+CREATE TABLE diagnoses
+(
+  id serial8 primary key,
+  disease_id INT8 references diseases(id),
+  patient_id INT8 references patients(id),
+  date_of_diagnosis VARCHAR(10), -- restrain to xx-xx-xxxx
+  severity_score INT1,
+  disease_active VARCHAR(5), -- restrict to true or false
+  date_of_resolution VARCHAR(10) -- restrain to xx-xx-xxxx
+);
